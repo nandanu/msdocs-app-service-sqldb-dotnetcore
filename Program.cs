9b23463239ae76d1702b02dfdeac59b49e17027e
nandanu <nandan.udiavar@gmail.com>
2025-02-09 06:31:44 +0000

    Hardcoded sql server connection string.

--- Program.cs
+++ Program.cs
@@ -12,7 +12,8 @@ if(builder.Environment.IsDevelopment())
 else
 {
     builder.Services.AddDbContext<MyDatabaseContext>(options =>
-        options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
+        // options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
+        options.UseSqlServer("Server=tcp:msdocs-core-sql-51-server.database.windows.net,1433;Database=msdocs-core-sql-51-database;Authentication=Active Directory Managed Identity");
     // builder.Services.AddStackExchangeRedisCache(options =>
     // {
     // options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
